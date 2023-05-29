c915cad6cafdb61107f57d2e49cc7653a5509c4e
denispsxnv <denisazizbekov@cloud.com>
2023-05-30 01:12:54 +0600

    last

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import styles from "../../styles/Sidebar.module.css";
+
+const Sidebar = () => {
+  const { list } = useSelector(({ categories }) => categories);
+
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          {list.map(({ id, name }) => (
+            <li key={id}>
+              <NavLink
+                className={({ isActive }) =>
+                  `${styles.link} ${isActive ? styles.active : ""}`
+                }
+                to={`/categories/${id}`}
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
